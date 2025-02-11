4bca4f0b7f83a70ffb2b1255525c7ce45322bfa9
Nakamura <124791730+GuiNakamuraC@users.noreply.github.com>
2025-02-11 12:47:23 -0300

    Add files via upload

---
+++ AULA01_11-02/calculamedia.php
@@ -0,0 +1,15 @@
+<?php
+    $nota1 = 8;
+    $nota2 = 7;
+    $nota3 = 9;
+    echo "NOTA 01: ".$nota1."<br>";
+    echo "NOTA 02: ".$nota2."<br>";
+    echo "NOTA 03: ".$nota3."<br>";
+    echo "<br>";
+    $media = 7;
+    if ($media >= 6) {
+        echo "Média: ".$media." - APROVADO!";
+    } else {
+        echo "Média: ".$media." - REPROVADO!";
+    }
+?>
